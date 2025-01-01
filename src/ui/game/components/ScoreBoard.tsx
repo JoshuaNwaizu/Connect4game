79a18b4eb2433568d425ca19a43fe276df2f84e1
JoshuaNwaizu <joshuachris000@gmail.com>
2025-01-01 08:56:40 +0100

    new changes

--- src/ui/game/components/ScoreBoard.tsx
+++ src/ui/game/components/ScoreBoard.tsx
@@ -15,7 +15,7 @@ const ScoreBoard = () => {
   };
 
   return (
-    <div className="flex justify-center gap-10 md:justify-center lg:hidden">
+    <div className="flex justify-center gap-8 md:justify-center lg:hidden">
       <motion.div
         initial="hidden"
         animate="visible"
